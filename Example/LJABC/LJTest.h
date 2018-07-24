f17128eeb1f13438c9868221a85a765ea7c7431f
handa <handa@ke.com>
2018-07-24 14:27:43 +0800

    add
    Change-Id: I834f086538fa49fb70f00920c0e12ac6d9c27aed

---
+++ Example/LJABC/LJTest.h
@@ -0,0 +1,13 @@
+//
+//  LJTest.h
+//  LJABC_Example
+//
+//  Created by 韩达 on 2018/7/23.
+//  Copyright © 2018年 shazideai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LJTest : NSObject
+
+@end
